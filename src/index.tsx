37a8792e35ce3a2a002a4f16c38dd1c412ab6bcf
taylercooper <trcooper1996@gmail.com>
2021-02-15 19:37:17 -0500

    Patterns can now be dragged and copied from the preview menu

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ type patternObj = {name: string, pattern: booleanGrid}
 enum GridType {MainGrid, Minigrid}
 
 
-class Game extends React.Component<{}, {size: number, board: booleanGrid, shadowBoard: booleanGrid, running: boolean, timer: NodeJS.Timeout, dragBuffer: booleanGrid}>{
+class Game extends React.Component<{}, {size: number, board: booleanGrid, shadowBuffer: booleanGrid, running: boolean, timer: NodeJS.Timeout, dragBuffer?: booleanGrid, currentPatternBuffered?: string}>{
   
   constructor(props: {}){
     super(props)
@@ -19,8 +19,16 @@ class Game extends React.Component<{}, {size: number, board: booleanGrid, shadow
       return Array(size).fill(false)
     })
 
+    const shadowBoard = board
+
     const timer = setInterval(() => this.playButtonHandler(), 500)
-    this.state = {size: size, board: board, running: false, timer: timer, dragBuffer: [[]], shadowBoard: [[]]}
+
+    this.BoardGrid = this.BoardGrid.bind(this)
+    this.PrefabsMenu = this.PrefabsMenu.bind(this)
+    this.PrefabColumn = this.PrefabColumn.bind(this)
+    this.PrefabPreview = this.PrefabPreview.bind(this)
+
+    this.state = {currentPatternBuffered: undefined, size: size, board: board, running: false, timer: timer, dragBuffer: undefined, shadowBuffer: shadowBoard}
   }
 
 
@@ -30,25 +38,69 @@ class Game extends React.Component<{}, {size: number, board: booleanGrid, shadow
     this.setState({board: board})
   }
 
-  clickHandlerPatternDrag(grid: booleanGrid){
-    this.setState({dragBuffer: grid})
+  clickHandlerPrefabGrid(grid: booleanGrid, name: string){
+    if (!this.state.currentPatternBuffered || this.state.currentPatternBuffered !== name){
+      this.setState({dragBuffer: grid, currentPatternBuffered: name})
+
+    }else if (this.state.currentPatternBuffered === name){
+      this.clearBufferAndShadow()
+
+    }
   }
 
-  test(){
-    return this.state.shadowBoard
+  clearBufferAndShadow(){
+    const emptyBoard = this.state.shadowBuffer.map(row => {
+      return row.map(_ => false)
+    })
+    this.setState({dragBuffer: undefined, currentPatternBuffered: undefined, shadowBuffer: emptyBoard})
   }
+
+  // Write the contents of the shadow buffer to the main grid.
+  // Maintain existing data
+  copyShadowBuffer(){
+    const sBoard = this.state.shadowBuffer
+    const board = this.state.board.map((row, rInd) => 
+      row.map((cell, cInd) => cell || sBoard[rInd][cInd])
+    )
+    this.setState({board: board})
+  }
+
 
   // This function is called when cells on the main board have a prefab dragged over them
-  clickHandlerCellDragEnter(row: number, column: number){
-    const size = this.state.size
-    const board = Array(size).fill(null).map((_, rowIndex) => {
-      return Array(size).fill(false).map((_, cIndex) =>{
-        return (rowIndex + row < size && cIndex + column < size && this.state.dragBuffer[rowIndex + row][cIndex + column])
-      })
+  // This function takes the cell that the mouse is currently on, in the form if its row and column, 
+  // and maps the cell pattern currently in the drag buffer onto the main grid, with the top-left most 
+  // alive cell in the dragBuffer being mapped onto the cell that the mouse is on.
+  cellHoverHandler(mouse_row: number, mouse_col: number){
+    if (this.state.dragBuffer){
+      const size = this.state.size
+      const dSize = this.state.dragBuffer.length
+      const dBuffer = this.state.dragBuffer
 
-    })
+      // Identify top-left most living cell in the dragBuffer
+      let offset = 0;
+      for (let i = 0; i < dSize; i ++){
+        if (dBuffer[i][i]){
+          offset = i;
+          break;
+        }
+      }
 
-    this.setState({shadowBoard: board})
+      // Get copy of 2d board.
+      const board = Array(size).fill(null).map(_ => Array(size).fill(false))
+
+      for (let row = 0; row < dSize; row ++){
+        for (let col = 0; col < dSize; col ++){
+          let finalRow = row + mouse_row - offset
+          let finalCol = col + mouse_col - offset
+          if (dBuffer[row][col] && board.length > finalRow && board[0].length > finalCol){
+            board[finalRow][finalCol] = true
+          }
+        }
+      }
+
+      this.setState({shadowBuffer: board})
+      }
+
   }
 
   flipRunningState(){
@@ -85,14 +137,14 @@ class Game extends React.Component<{}, {size: number, board: booleanGrid, shadow
   }
 
   renderBoard(){
-    const contrivedOnClick = () => {}
-    return <this.BoardGrid onClick={contrivedOnClick} grid={this.state.board} gridType={GridType.MainGrid}/>
+    return <this.BoardGrid grid={this.state.board} gridType={GridType.MainGrid}/>
   }
 
   PrefabsMenu(){
   return(
       <div id={"prefabMenu"}>
         {patterns.map((patternGroup) => {
+          
           return <this.PrefabColumn groupName={patternGroup.groupName} pattern={patternGroup.patterns}/>
         })}
       </div>
@@ -104,8 +156,11 @@ PrefabColumn(props: {groupName: string, pattern: patternObj[]}){
     <div className={"prefabColumn"}>
       <p>{props.groupName}</p>
       <ul>
-        {props.pattern.map((val) => {
-          return <li className={"prefabPreview"} key={val.name}><this.PrefabPreview name={val.name} pattern={val.pattern}/></li>
+        {props.pattern.map((val, index) => {
+          const highlightedTag = (this.state.currentPatternBuffered === val.name) ? "highlightedPrefabPreview" : ""
+          return <li className={"prefabPreview"} id={highlightedTag} key={val.name} onClick={() => this.clickHandlerPrefabGrid(val.pattern, val.name)}>
+            <this.PrefabPreview name={val.name} pattern={val.pattern}/>
+            </li>
         })}
       </ul>
     </div>
@@ -116,51 +171,56 @@ PrefabPreview(props: {name: string, pattern: booleanGrid}){
   return (
     <>
     <div className={"miniBoardGridContainer"}>
-    <this.BoardGrid grid={props.pattern} gridType={GridType.Minigrid} onClick={() => this.clickHandlerPatternDrag(props.pattern)}/>
+    <this.BoardGrid grid={props.pattern}  gridType={GridType.Minigrid}/>
     </div>
     <p>{props.name}</p>
     </>
   )
 }
 
-    BoardGrid(props: {grid: booleanGrid, gridType: GridType, onClick: () => void}){
-                              //onClickMain?: ((rIndex: number, cIndex: number) => void),
-                              //onClickMini?: (() => void) }){
+  BoardGrid(props: {grid: booleanGrid, gridType: GridType}){
 
-      const isMainBoard = props.gridType === GridType.MainGrid
-      const className = (isMainBoard) ? "mainBoard boardGrid" : "miniBoard boardGrid"
-      const shadowBoard = this.test()
-      // Can't figure out how to adjust BoardGrid to take the union of the two required function types.
-      // So, this function takes two functions everytime. one of the functions will actually have a body,
-      // and the other will be one of the dummy functions below.
-      let onClickMini = (isMainBoard) ? () => props.onClick() : undefined
-      let onDragEnd = () => this.clickHandlerPatternDrag([[]])
+    const isMainBoard = props.gridType === GridType.MainGrid
+    const className = (isMainBoard) ? "mainBoard boardGrid" : "miniBoard boardGrid"
+    const shadowBoard = this.state.shadowBuffer
 
 
+    return(
+      <div className={className}>{
+      props.grid.map((rowArray, rowIndex) => {
+        return( 
+          <ul className={"boardRow"}>{
+          rowArray.map((c, cIndex) => {
+            let className = c ? "alive cell" : "dead cell";
+            if (isMainBoard && shadowBoard[rowIndex][cIndex]){
+              className = className + " shadow"
+            }
 
-      return(
-        <div className={className} onDragStart={onClickMini} onDragEnd={onDragEnd}>{
-        props.grid.map((rowArray, rowIndex) => {
-          return( 
-            <ul className={"boardRow"}>{
-            rowArray.map((c, cIndex) => {
-              let className = c ? "alive cell" : "dead cell";
-              if (shadowBoard[rowIndex][cIndex]){
-                className = className + " shadow"
+            let onMouseEnter = undefined
+            let onClickMain = undefined
+            // Only the main board has an event attatched to the <li> element.
+            if (isMainBoard){
+              if (this.state.currentPatternBuffered){
+                onMouseEnter = () => this.cellHoverHandler(rowIndex, cIndex)
+                onClickMain = () => this.copyShadowBuffer()
+              }else{
+                onClickMain = () => this.clickHandler(rowIndex, cIndex)
               }
-              const onClickMain = (isMainBoard) ? () => this.clickHandler(rowIndex, cIndex) : undefined
-              return(
-                <li key={rowIndex*rowArray.length + cIndex} className={className} onClick={onClickMain} onDragEnter={() => this.clickHandlerCellDragEnter(rowIndex, cIndex)}>
-                  {
-                  }
-                </li>
-              )
-            })}
-            </ul>
-        )})}
-          </div>
-          )
-    }
+            }
+
+            //const onClickMain = (isMainBoard) ? () => this.clickHandler(rowIndex, cIndex) : undefined
+            return(
+              <li key={rowIndex*rowArray.length + cIndex} className={className} onClick={onClickMain} onMouseEnter={onMouseEnter}>
+                {
+                }
+              </li>
+            )
+          })}
+          </ul>
+      )})}
+        </div>
+        )
+  }
 
 
   render(){
@@ -198,6 +258,7 @@ function judge(isAlive: boolean, neighbors: number){
   //Otherwise, dead cell
   return false
 }
+
 
 
 ReactDOM.render(
